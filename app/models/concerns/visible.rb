da2c781a8ca9579aed7637ad209cefddc2135cd7
Nabin Sah <nabinsah703@gmail.comgit config --global user.email nabinsah703@gmail.com>
2022-03-28 11:09:28 +0545

    ruby on rails guide web development

---
+++ app/models/concerns/visible.rb
@@ -0,0 +1,16 @@
+module Visible
+    extend ActiveSupport::Concern
+    VALID_STATUSES = ['public', 'private', 'archived']
+
+    included do
+        validates :status, inclusion: { in: VALID_STATUSES }
+    end
+    
+
+
+
+
+    def archived?
+        status == 'archieved'
+    end
+end
